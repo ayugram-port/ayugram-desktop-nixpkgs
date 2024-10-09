9e3d91211674e884ec86094749981d3bb6b8cc94
kaeeraa <kaeeraa@nebula-nook.ru>
2024-10-09 20:55:40 +0300

    flake: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Desktop Telegram client with good customization and Ghost mode.";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  nixConfig = {
+    # cache, if user trust me
+    extra-substituters = [ "https://cache.garnix.io" ];
+    extra-trusted-public-keys = [ "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" ];
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = rec {
+          ayugram-desktop = (pkgs.libsForQt5.callPackage ./package.nix { });
+          default = ayugram-desktop;
+        };
+      }
+    );
+}
